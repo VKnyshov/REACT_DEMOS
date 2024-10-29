f34fa941477f0130a4c723b1ed9ff8fe66e69c6d
VKnyshov <valeriy.knyshov@gmail.com>
2024-10-29 21:26:51 +0200

    all in

--- src/components/CommentsComponent.tsx
+++ src/components/CommentsComponent.tsx
@@ -1,9 +1,33 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import {ICommentModel} from "../models/ICommentModel";
+import {commentService} from "../services/api.service";
 
 const CommentsComponent = () => {
+
+    const [comments, setComments] = useState<ICommentModel[]>([])
+    useEffect(() => {
+        commentService.getComments()
+            .then(value => {
+                console.log(value.data)
+                setComments(value.data)
+            })
+    }, []);
     return (
         <div>
-            CommentsComponent
+            {
+                comments.map((comment: ICommentModel, index) => (
+                    <div key={index}>
+                        <p>Comment ID - {comment.id}</p>
+                        <p>Post ID - {comment.postId}</p>
+                        <p>Email - {comment.email}</p>
+                        <h4>Comment Name - {comment.name}</h4>
+                        <p>Comment body - {comment.body}</p>
+<hr/>
+<hr/>
+
+                    </div>
+                ))
+            }
         </div>
     );
 };
