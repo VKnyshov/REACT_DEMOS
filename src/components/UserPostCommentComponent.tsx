9b928c87e02e9038e08b466c2bdab4060e3abdb5
VKnyshov <valeriy.knyshov@gmail.com>
2024-11-09 14:18:39 +0200

    all in

--- src/components/UserPostCommentComponent.tsx
+++ src/components/UserPostCommentComponent.tsx
@@ -4,6 +4,7 @@ import {IUser} from "../models/IUser";
 import {userActions} from "../slices/userSlice";
 import {postActions} from "../slices/postSlice";
 import {commentActions} from "../slices/commentSlice";
+import {IPost} from "../models/IPost";
 
 const UserPostCommentComponent = () => {
 
@@ -21,24 +22,51 @@ const UserPostCommentComponent = () => {
 
     return (
         <div>
-            {
-                users.map((user: IUser, index) =>
-                    (<div key={index}> {user.id}. {user.name}</div>))
-            }
-            <hr/>
-            <hr/>
-            {
-                posts.map((post, index) =>
-                    (<div key={index}>{post.id}. {post.title}</div>))
 
-            }
-            <hr/>
-            <hr/>
-            {
-                comments.map((comment, index) =>
-                    (<div key={index}>{comment.id}. {comment.body}</div>)
-                )
-            }
+            {users.map((user: IUser) => (
+
+                <div key={user.id}>
+                    <h3>{user.id}. {user.name}</h3>
+                    <ul>
+                        {posts.filter(post => post.userId === user.id)
+                            .map((post: IPost, index) => (
+                                <li key={index}>{post.id}. {post.title}
+
+                                    <ul>
+                                        {comments.filter(comment => comment.postId === post.id)
+                                            .map((comment, index) => (
+                                                <li key={index}>{comment.body}</li>
+                                            ))}
+                                    </ul>
+                                </li>
+                            ))}
+
+                    </ul>
+                    <hr/>
+                    <hr/>
+
+                </div>
+            ))}
+
+
+            {/*{*/}
+            {/*    users.map((user: IUser, index) =>*/}
+            {/*        (<div key={index}> {user.id}. {user.name}</div>))*/}
+            {/*}*/}
+            {/*<hr/>*/}
+            {/*<hr/>*/}
+            {/*{*/}
+            {/*    posts.map((post, index) =>*/}
+            {/*        (<div key={index}>{post.id}. {post.title}</div>))*/}
+
+            {/*}*/}
+            {/*<hr/>*/}
+            {/*<hr/>*/}
+            {/*{*/}
+            {/*    comments.map((comment, index) =>*/}
+            {/*        (<div key={index}>{comment.id}. {comment.body}</div>)*/}
+            {/*    )*/}
+            {/*}*/}
         </div>
     );
 };
